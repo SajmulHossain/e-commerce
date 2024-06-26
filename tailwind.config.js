1358d5b33f32c3db2851e07b87a77a1513536c76
Sajmul <sajmul1427@gmail.com>
2024-06-26 21:39:39 +0600

    as

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./admin-panel/**/*.{html,css,js}'],
+  content: ["./admin-panel/**/*.{html,js}"],
   theme: {
     extend: {},
   },
